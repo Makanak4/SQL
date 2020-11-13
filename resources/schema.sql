e2ad265076b4de75adb198ea1cc3df11f6ee3538
Makanak4 <losos43rus@gmail.com>
2020-11-13 19:37:53 +0300

    add table

---
+++ resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Flats (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    name  TEXT NOT NULL,
+    price INTEGER NOT NULL CHECK ( price > 0 ),
+    countRoom INTEGER NOT NULL CHECK ( countRoom > 0 ),
+    station_id INTEGER REFERENCES Stations (id),
+    owner_id INTEGER REFERENCES Owners (id)
+);
+
+CREATE TABLE Owners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE Stations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
+
