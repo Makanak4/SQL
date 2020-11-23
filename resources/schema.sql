373e0875a5e1ddc2e16f10b23a1cf6c3d1bb2b6f
Makanak4 <losos43rus@gmail.com>
2020-11-23 21:46:42 +0300

    #fixed 1

--- resources/schema.sql
+++ resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Flats (
     id    INTEGER PRIMARY KEY AUTOINCREMENT,
     name  TEXT NOT NULL,
     price INTEGER NOT NULL CHECK ( price > 0 ),
-    countRoom INTEGER NOT NULL CHECK ( countRoom > 0 ),
+    count_room INTEGER NOT NULL CHECK ( count_room > 0 ),
     station_id INTEGER REFERENCES Stations (id),
     owner_id INTEGER REFERENCES Owners (id)
 );
